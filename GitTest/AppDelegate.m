52e92bcc2360223a9e4530c95c0ee76965f59876
江秋朋 <jiangqiupeng@jiangqiupengdemacbook-pro.local>
2016-05-03 17:30:06 +0800

    愚蠢的错误！！

--- GitTest/AppDelegate.m
+++ GitTest/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"so Stupid!!!");
     return YES;
 }
 
